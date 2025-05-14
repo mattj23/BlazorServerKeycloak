459c689875cdd09f625ae348fb221e3b95e9e317
Robert Cato <saiwolf@swmnu.net>
2025-05-14 13:55:31 -0500

    Refactor cookie authentication event handling
    Moved `OnValidatePrincipal` and `OnTokenValidated` logic
    to a new `AuthEventHandlers` class for better organization.
    
    The methods now handle token validation and refresh logic,
    simplifying the event handling in the `AddKeycloak` method.

--- BlazorServerKeycloak/Extensions.cs
+++ BlazorServerKeycloak/Extensions.cs
@@ -32,38 +32,8 @@ public static class Extensions
                 options.SlidingExpiration = true;
                 options.Events = new CookieAuthenticationEvents
                 {
-                    OnValidatePrincipal = async c =>
-                    {
-                        // this event is fired everytime the cookie has been validated by the cookie middleware, so basically during every authenticated request.
-                        // the decryption of the cookie has already happened so we have access to the identity + user claims
-                        // and cookie properties - expiration, etc..
-                        // source: https://github.com/mderriey/aspnet-core-token-renewal/blob/2fd9abcc2abe92df2b6c4374ad3f2ce585b6f953/src/MvcClient/Startup.cs#L57
-                        var now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-                        var expires = c.Properties.ExpiresUtc.GetValueOrDefault().ToUnixTimeSeconds();
-
-                        if (now >= expires)
-                        {
-                            var response = await new HttpClient().RequestRefreshTokenAsync(new RefreshTokenRequest
-                            {
-                                Address = oidcOptions.Authority +
-                                          "/protocol/openid-connect/token",
-                                ClientId = oidcOptions.ClientId,
-                                ClientSecret = oidcOptions.ClientSecret,
-                                RefreshToken =
-                                    ((ClaimsIdentity)c.Principal!.Identity!)
-                                    .GetClaimValue(ClaimType
-                                        .RefreshToken)
-                            }).ConfigureAwait(false);
-
-                            if (!response.IsError)
-                            {
-                                ((ClaimsIdentity)c.Principal!.Identity!)
-                                    .SetIdentityClaims(response.AccessToken, response.RefreshToken);
-
-                                c.ShouldRenew = true;
-                            }
-                        }
-                    }
+                    OnValidatePrincipal = async (context) 
+                        => await AuthEventHandlers.OnValidatePrincipalAsync(context, oidcOptions)
                 };
             })
             .AddOpenIdConnect(options =>
@@ -85,26 +55,12 @@ public static class Extensions
                 options.Scope.Clear();
                 foreach (var scope in oidcOptions.Scope) options.Scope.Add(scope);
 
-                options.Events = new OpenIdConnectEvents
+                options.Events = new()
                 {
-                    OnTokenValidated = t =>
-                    {
-                        // this event is called after the OIDC middleware received the authorization code,
-                        // redeemed it for an access token + a refresh token and validated the identity token
-                        ((ClaimsIdentity)t.Principal!.Identity!)
-                            .SetIdentityClaims(t.TokenEndpointResponse!.AccessToken!,
-                                t.TokenEndpointResponse.RefreshToken);
-
-                        t.Properties!.ExpiresUtc =
-                            new JwtSecurityToken(t.TokenEndpointResponse.AccessToken)
-                                .ValidTo; // align expiration of the cookie with expiration of the access token
-                        t.Properties.IsPersistent =
-                            true; // so that we don't issue a session cookie but one with a fixed expiration
-
-                        return Task.CompletedTask;
-                    }
+                    OnTokenValidated = (context) 
+                        => AuthEventHandlers.OnTokenValidated(context)
                 };
-                options.TokenValidationParameters = new TokenValidationParameters
+                options.TokenValidationParameters = new()
                 {
                     NameClaimType = "name",
                     RoleClaimType = "groups",
